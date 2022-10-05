8168256e23cdfa70198df1b1806dd365b388f916
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-05 18:29:49 +0200

    comenzando

--- analisisdedatos.py
+++ analisisdedatos.py
@@ -3,6 +3,14 @@ import numpy as np
 import numpy.ma as ma
 import matplotlib
 
+DATABASE_PATH = "club.csv"
 
 class DataBase:
      
+    df = pd.read_csv(DATABASE_PATH)
+    print(df)
+    price=[]
+    price=df['Market Value Of Club In Millions(£)']
+    print(price)
+    players=df['Market Value Of Top 18 Players In Millions(£)']
+
