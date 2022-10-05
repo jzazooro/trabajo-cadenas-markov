c572039a370dabe9303bd1179fba9521d963f111
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-05 20:44:38 +0200

    jfajokfdjkofjodskjsdfjk

--- analisisdedatos.py
+++ analisisdedatos.py
@@ -9,9 +9,12 @@ class DataBase:
      
     df = pd.read_csv(DATABASE_PATH)
     print(df)
-    price=[]
     price=df['Market Value Of Club In Millions(£)'].tolist()
     print(price)
     players=df['Average Age Of Players'].tolist()
     print(players)
-
+    div=[]
+    for i in price:
+        for j in players:
+            x=i/j
+            div.append(x)
