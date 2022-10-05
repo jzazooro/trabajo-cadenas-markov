abe81d97b69d068e2c6fe500a79c88b0749b6bcd
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-05 23:23:00 +0200

    añadidas preguntas y el algoritmo iniciado

--- analisisdedatos.py
+++ analisisdedatos.py
@@ -27,6 +27,21 @@ for i in range(len(price)):
 print(lista_cb)
 
 #lista_cb[nombres][($)/media_edad]
-
+print('=================================')
+print('        TODOS LOS CLUBES')
+print('      Y SUS PRECIO/MEDIA EDAD')
+print('=================================')
 for i in range(len(lista_cb)):
-    print(f'Club: {lista_cb[i][0]}; Precio/Edad: {lista_cb[i][1]}')
+    print(f'Club: {lista_cb[i][0]}\nPrecio/Edad: {lista_cb[i][1]}\n\n')
+print('=================================\n\n')
+
+pre1=input('Elegir club para comparar (1): ')
+pre2=input('Elegir club para comparar (2): ')
+
+temp1=0
+temp2=0
+
+for i in lista_cb:
+    for j in range(len(lista_cb)):
+        if(lista_cb[j][0]==pre1):
+            temp1=lista_cb[j][1]
