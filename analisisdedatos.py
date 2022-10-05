fba1d752591e19f4bc7e7e6a904690288f477d1f
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-05 23:01:04 +0200

    codigo que he hecho en el bus

--- analisisdedatos.py
+++ analisisdedatos.py
@@ -5,16 +5,29 @@ import matplotlib
 
 DATABASE_PATH = "club.csv"
 
-class DataBase:
-     
-    df = pd.read_csv(DATABASE_PATH)
-    print(df)
-    price=df['Market Value Of Club In Millions(£)'].tolist()
-    print(price)
-    players=df['Average Age Of Players'].tolist()
-    print(players)
-    div=[]
-    for i in price:
-        for j in players:
-            x=i/j
-            div.append(x)
+
+def calcula_prob(a,b):
+    a=a[1]
+    b=b[1]
+    a_gana=float(a/(a+b+2))
+    b_gana=float(b/(a+b+2))
+    empate= 1-(a_gana)-(b_gana)
+    
+df = pd.read_csv(DATABASE_PATH)
+price=df['Market Value Of Club In Millions(£)'].tolist()
+clubes=df['Club Name'].tolist()
+players=df['Average Age Of Players'].tolist()
+div=[]
+
+for i in range(len(price)):
+    x=price[i]/players[i]
+    div.append(x)
+print(div)
+
+lista_cb=[]
+
+for i in range(len(price)):
+    lista_cb.append([clubes[i],div[i]])
+print(lista_cb)
+
+print(lista_cb[0][1])
